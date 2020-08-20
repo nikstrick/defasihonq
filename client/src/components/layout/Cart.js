eec5eb07ca9a9a2e84e214aafd321d82e29e272a
Nikhil Ranjan <nikhilranjan227@gmail.com>
2020-08-20 19:34:56 +0100

    Order issue successfully handled

--- client/src/components/layout/Cart.js
+++ client/src/components/layout/Cart.js
@@ -1,4 +1,4 @@
-import React ,{useEffect,useState}from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -9,7 +9,7 @@ import Typography from '@material-ui/core/Typography';
 import { Redirect } from 'react-router-dom';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import { connect } from 'react-redux';
-import { Container,Col, Row} from 'reactstrap';
+import { Container, Col, Row } from 'reactstrap';
 import { Progress } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { logout } from '../../actions/auth';
@@ -17,11 +17,10 @@ import { deleteCartItem } from '../../actions/cartActions';
 import axios from 'axios';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    width:'100%'
+    width: '100%',
   },
   details: {
     display: 'flex',
@@ -31,7 +30,7 @@ const useStyles = makeStyles((theme) => ({
     flex: '1 0 auto',
   },
   cover: {
-    width: "50%",
+    width: '50%',
   },
   controls: {
     display: 'flex',
@@ -45,123 +44,126 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Cart = ({deleteCartItem,auth}) =>{
+const Cart = ({ deleteCartItem, auth, product }) => {
   const { user } = auth;
   const classes = useStyles();
   const theme = useTheme();
-   const onDeleteClick = (id) => {
-      deleteCartItem(id);
-      console.log(id);
-          const func = async() =>{
-          const result = await axios.get('api/users/cart');
-          console.log(result);
-          setData(result.data);
-      
-          let tot = 0;
-          result.data.map((item)=>{tot+=(item.quantity * item.price)});
-          setTotal(tot);
-          }
-          func();
-        
-      }
-    const [data, setData] = useState([]);
-    const [total, setTotal] = useState(0);
- 
-  useEffect( () => {
-    const func = async() =>{
-    const result = await axios.get('api/users/cart');
-    console.log(result);
-    setData(result.data);
+  const onDeleteClick = (id) => {
+    deleteCartItem(id);
+    console.log(id);
+    const func = async () => {
+      const result = await axios.get('api/users/cart');
+      console.log(result);
+      setData(result.data);
 
-    let tot = 0;
-    result.data.map((item)=>{tot+=(item.quantity * item.price)});
-    setTotal(tot);
-    }
+      let tot = 0;
+      result.data.map((item) => {
+        tot += item.quantity * item.price;
+      });
+      setTotal(tot);
+    };
+    func();
+  };
+  const [data, setData] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const func = async () => {
+      const result = await axios.get('api/users/cart');
+      console.log(result);
+      setData(result.data);
+
+      let tot = 0;
+      result.data.map((item) => {
+        tot += item.quantity * item.price;
+      });
+      setTotal(tot);
+    };
     func();
   }, []);
   if (!auth.isAuthenticated) {
     return <Redirect to='/' />;
   }
-  return(
-      <div>
+  return (
+    <div>
       <div className='back-to-result'>
-          <Link to='/'>Back to Home</Link>
-        </div>
-    <Progress striped color="warning" value={25} />
-    <Container fluid={true}> 
+        <Link to='/'>Back to Home</Link>
+      </div>
+      <Progress striped color='warning' value={25} />
+      <Container fluid={true}>
         <Row>
-        <h1 className='large page-text'>Cart</h1>
+          <h1 className='large page-text'>Cart</h1>
         </Row>
         <Row>
-    <Col xs="12" md="6"><div className="cartback"></div></Col>
-     <Col xs="12" md="6">
-   
-           <div className="detailsDark">Total Amount: {total}</div>
-    
-          
-          {data.map((item, index) => (
-                <div>
+          <Col xs='12' md='6'>
+            <div className='cartback'></div>
+          </Col>
+          <Col xs='12' md='6'>
+            <div className='detailsDark'>Total Amount: {total}</div>
+
+            {data.map((item, index) => (
+              <div>
                 <Card className={classes.root}>
-                <div className={classes.details}>
-                  <CardContent className={classes.content}>
-                    <Typography component="h5" variant="h5">
-                      {item.name}
-                    </Typography>
-                    <Typography variant="subtitle1" color="textSecondary">
-                    <b>Price:</b>₹{item.price}
-                    
-                    </Typography>
-                    
-                    <Typography variant="subtitle1" color="textSecondary">
-                    <b>MRP:</b>₹{item.mrp}
-                    
-                    </Typography>
-                    <Typography variant="subtitle1" color="textSecondary">
-                    <b>Quantity:</b>
-                     {item.quantity}
-                    
-                    </Typography>
-                    <Typography variant="subtitle1" color="textSecondary">
-                    <b>
+                  <div className={classes.details}>
+                    <CardContent className={classes.content}>
+                      <Typography component='h5' variant='h5'>
+                        {item.name}
+                      </Typography>
+                      <Typography variant='subtitle1' color='textSecondary'>
+                        <b>Price:</b>₹{item.price}
+                      </Typography>
+
+                      <Typography variant='subtitle1' color='textSecondary'>
+                        <b>MRP:</b>₹{item.mrp}
+                      </Typography>
+                      <Typography variant='subtitle1' color='textSecondary'>
+                        <b>Quantity:</b>
+                        {item.quantity}
+                      </Typography>
+                      <Typography variant='subtitle1' color='textSecondary'>
+                        <b>
                           <div>Total:₹{item.quantity * item.price}</div>
-                    </b>
-                    
-                    </Typography>
-                  </CardContent>
-                </div>
-                <CardMedia
-                  className={classes.cover}
-                  image="https://beebom.com/wp-content/uploads/2020/06/mi-notebook-14-horizon-edition.jpg"
-                  title="Live from space album cover"
-                />
-                <div>
-              
-                <IconButton aria-label="delete" onClick={()=>onDeleteClick(item._id)}>
-                <DeleteIcon />
-                 </IconButton>
+                        </b>
+                      </Typography>
+                    </CardContent>
+                  </div>
+                  <CardMedia
+                    component='img'
+                    alt={item.name}
+                    className={classes.cover}
+                    src={item.image}
+                    title={item.name}
+                  />
+                  <div>
+                    <IconButton
+                      aria-label='delete'
+                      onClick={() => onDeleteClick(item._id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </div>
+                </Card>
               </div>
-              </Card>
-              
-            </div>
-              
-          ))}
-            
-      <Link to="/Address" className="btn btn-dark">Add Address</Link>
-     
-    </Col>
-    </Row>
-  </Container>
-  </div>
+            ))}
+
+            <Link to='/Address' className='btn btn-dark'>
+              Add Address
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
-}
+};
 Cart.propTypes = {
   auth: PropTypes.object.isRequired,
   logout: PropTypes.func.isRequired,
   deleteCartItem: PropTypes.func.isRequired,
+  product: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
+  product: state.product,
 });
 
-export default connect(mapStateToProps,{ logout, deleteCartItem })(Cart);
+export default connect(mapStateToProps, { logout, deleteCartItem })(Cart);
--- client/src/components/layout/Payment.js
+++ client/src/components/layout/Payment.js
@@ -1,8 +1,17 @@
-import React ,{useEffect,useState}from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Container,Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import {
+  Container,
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from 'reactstrap';
 import { deleteCart } from '../../actions/cartActions';
 import { deleteAddress } from '../../actions/address';
 import { Toast, ToastBody, ToastHeader } from 'reactstrap';
@@ -10,13 +19,19 @@ import { Progress } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { CART_FAIL } from '../../actions/types';
-const Payment = ({isAuthenticated,deleteCart,deleteAddress,address,cart}) =>{
-  const [pay,setPay] = useState("cod");
-  const handleChange = (e) =>{
-       setPay(e.target.value);
-  }
+const Payment = ({
+  isAuthenticated,
+  deleteCart,
+  deleteAddress,
+  address,
+  cart,
+}) => {
+  const [pay, setPay] = useState('cod');
+  const handleChange = (e) => {
+    setPay(e.target.value);
+  };
 
-  const handleClick = async () =>{
+  const handleClick = async () => {
     //post order
     console.log('order posted');
     const config = {
@@ -24,86 +39,102 @@ const Payment = ({isAuthenticated,deleteCart,deleteAddress,address,cart}) =>{
         'Content-Type': 'application/json',
       },
     };
-    let tot=0;
+    let tot = 0;
+    const result = await axios.get('api/users/cart');
     console.log(cart);
-    cart.cart.map((e)=>{tot+=(e.quantity * e.price)});
+    result.data.map((e) => {
+      tot += e.quantity * e.price;
+    });
     console.log(tot);
-    const body = JSON.stringify({ orderItems:cart.cart, paymentMethod:"COD" ,address,orderTotal:tot });
+    const body = JSON.stringify({
+      orderItems: result.data,
+      paymentMethod: 'COD',
+      address,
+      orderTotal: tot,
+    });
     console.log(body);
     const res = await axios.post('/api/order', body, config);
     console.log(res);
     //cart empty
     //user cart empty
-    
+
     deleteCart();
     //delete address
     deleteAddress();
-    
-  }
+  };
 
   if (!isAuthenticated) {
     return <Redirect to='/' />;
   }
-  return(
-      <div>
-    <Progress striped color="warning" value={75} />
-    <Container fluid={true}> 
+  return (
+    <div>
+      <Progress striped color='warning' value={75} />
+      <Container fluid={true}>
         <Row>
-        <h1 className='large page-text'>Select Payment Method</h1>
+          <h1 className='large page-text'>Select Payment Method</h1>
         </Row>
         <Row>
-    <Col xs="12" md="6"><div className="paymentback"></div></Col>
-     <Col xs="12" md="6">
-     
-     
-     <FormGroup tag="fieldset">
-        <FormGroup check>
-          <Label check>
-            <Input onChange={handleChange} type="radio" name="radio1" value="cod" checked={pay==="cod"} />{' '}
-            <Toast>
-          <ToastHeader>
-            Cash on Delivery(COD)
-          </ToastHeader>
-          <ToastBody>
-            Pay us after you get the product!!
-          </ToastBody>
-        </Toast>
-          </Label>
-        </FormGroup>
-        <FormGroup check disabled >
-          <Label check>
-            <Input onChange={handleChange} type="radio" name="radio1" value="cdc" checked={pay==="cdc"} disabled/>{' '}
-            <Toast>
-          <ToastHeader>
-            Credit/Debit Cards
-          </ToastHeader>
-          <ToastBody>
-            Coming Soon!! Please select COD           
-          </ToastBody>
-        </Toast>
-            
-          </Label>
-        </FormGroup>
-      </FormGroup>
-      
-      <Link onClick = {handleClick} to="/orderconfirm" className="btn btn-dark">Place Order</Link>
-    </Col>
-    </Row>
-  </Container>
-  </div>
+          <Col xs='12' md='6'>
+            <div className='paymentback'></div>
+          </Col>
+          <Col xs='12' md='6'>
+            <FormGroup tag='fieldset'>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    onChange={handleChange}
+                    type='radio'
+                    name='radio1'
+                    value='cod'
+                    checked={pay === 'cod'}
+                  />{' '}
+                  <Toast>
+                    <ToastHeader>Cash on Delivery(COD)</ToastHeader>
+                    <ToastBody>Pay us after you get the product!!</ToastBody>
+                  </Toast>
+                </Label>
+              </FormGroup>
+              <FormGroup check disabled>
+                <Label check>
+                  <Input
+                    onChange={handleChange}
+                    type='radio'
+                    name='radio1'
+                    value='cdc'
+                    checked={pay === 'cdc'}
+                    disabled
+                  />{' '}
+                  <Toast>
+                    <ToastHeader>Credit/Debit Cards</ToastHeader>
+                    <ToastBody>Coming Soon!! Please select COD</ToastBody>
+                  </Toast>
+                </Label>
+              </FormGroup>
+            </FormGroup>
+
+            <Link
+              onClick={handleClick}
+              to='/orderconfirm'
+              className='btn btn-dark'>
+              Place Order
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
-}
+};
 Payment.propTypes = {
   orders: PropTypes.array.isRequired,
-  isAuthenticated: PropTypes.bool
+  isAuthenticated: PropTypes.bool,
 };
 
-const mapStateToProps = state => ({
-  orders:state.orders,
+const mapStateToProps = (state) => ({
+  orders: state.orders,
   isAuthenticated: state.auth.isAuthenticated,
-  auth:state.auth,
-  address:state.address,
-  cart:state.cart
+  auth: state.auth,
+  address: state.address,
+  cart: state.cart,
 });
 
-export default connect(mapStateToProps,{deleteAddress,deleteCart})(Payment);
+export default connect(mapStateToProps, { deleteAddress, deleteCart })(Payment);
