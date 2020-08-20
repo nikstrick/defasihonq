caafdb73524e1b959f7e09bb589c56b4467192f7
Nikhil Ranjan <nikhilranjan227@gmail.com>
2020-08-20 16:11:04 +0100

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
-
+const path = require('path');
 
 connectDB();
+//Init middleware
+app.use(express.json({ extended: false }));
 
-app.use(express.json({extended:false}));
+//Define routes
 
-app.get('/',(req,res)=>res.send(`Api running`)); 
-
-app.use('/api/otp',require('./routes/api/otp'));
-app.use('/api/users',require('./routes/api/users'));
-app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/otp', require('./routes/api/otp'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/product', require('./routes/api/product'));
-app.use('/api/order',require('./routes/api/order'));
+app.use('/api/order', require('./routes/api/order'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
